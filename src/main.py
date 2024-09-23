c799ab0505df45641e83b81a85dcb848726912dc
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-09-24 00:53:49 +0100

    Create output data

--- src/main.py
+++ src/main.py
@@ -19,6 +19,8 @@ programmes = []
 for page in pages:
     programmes += page["Data"]
 
+output_data = [["Job Code", "Deanery", "Programme Type", "F1 Hospital Trust", "F2 Hospital Trust", "Specialities"]]
+
 for programme in programmes:
     job_code = programme["ProgrammePreference"]
     deanery = programme["Deanery"]
@@ -32,3 +34,14 @@ for programme in programmes:
     f2_hospital_trust = (title.group('f2') or "").strip()
 
     specialities = programme["Specialties"].split(", ")
+
+    row = [
+        job_code,
+        deanery,
+        programme_type,
+        f1_hospital_trust,
+        f2_hospital_trust,
+    ]
+    for speciality in specialities:
+        row.append(speciality)
+    output_data.append(row)
