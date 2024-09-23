df2c28bb6c7f2e1e20b7f83cc011fc325ad746ff
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-09-23 18:01:36 +0100

    Read input data

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
+from pathlib import Path
+
 print("MedScript\n=========")
+
+pages = []
+for page_number in range(1, 12):
+    path = Path(__file__).parent / f"../data/page{page_number}.json"
+    with open(path) as f: pages.append(f.read())
