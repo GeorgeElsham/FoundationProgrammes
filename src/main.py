99bb0304d60cb731836ab28a593a48ede4956d5c
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-09-23 18:42:34 +0100

    Load as JSON data

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from pathlib import Path
+import json
 
 print("MedScript\n=========")
 
 pages = []
 for page_number in range(1, 12):
     path = Path(__file__).parent / f"../data/page{page_number}.json"
-    with open(path) as f: pages.append(f.read())
+    with open(path) as f:
+        page_str = f.read()
+        page = json.loads(page_str)
+        pages.append(page)
