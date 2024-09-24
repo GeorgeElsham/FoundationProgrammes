3a9ca4b115e88264c98ebeb7f8f2b613e6e5ab20
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-09-24 02:21:56 +0100

    Match when text is before too

--- src/main.py
+++ src/main.py
@@ -58,13 +58,15 @@ for programme in programmes:
     f2_title = (title_match.group('f2') or "").strip()
 
     description = programme["ProgrammeDescription"]
-    description_pattern = r"F1 - (?P<f1>.+)\.\s+F2 - (?P<f2>.+)\.\s+(?P<rest>.*)"
+    description_pattern = r"(?P<start>(?:(?!F1 - ).)*)F1 - (?P<f1>.+)\.\s+F2 - (?P<f2>.+)\.\s+(?P<end>.*)"
     description_regex = re.compile(description_pattern)
     description_match = description_regex.match(description)
     if description_match is not None:
         f1_title = description_match.group('f1').strip()
         f2_title = description_match.group('f2').strip()
-        description = description_match.group('rest').strip()
+        start = description_match.group('start').strip()
+        end = description_match.group('end').strip()
+        description = f"[SHORTENED]: {start} {end}"
 
     specialities = programme["Specialties"].split(", ")
 
